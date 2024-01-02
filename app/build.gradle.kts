6eafd0efbcae8718c336c13e7a26377b4a3d4d0c
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-01-03 00:55:45 +0100

    Bump target SDK level to 23
    The minimum to allow normal installation on Android 14.
    
    From reading through the notes in Build.VERSION_CODES, we shouldn't be
    negatively impacted by any behaviour changes, but this probably needs a bit of
    testing.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         applicationId = "de.buttercookie.units"
         minSdk = 4
         //noinspection ExpiredTargetSdkVersion
-        targetSdk = 14
+        targetSdk = 23
         versionCode = 10
         versionName = "1.1a1"
     }
