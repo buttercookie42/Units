6d4b33314a4ae8e65d3928a6591af47b826caa56
Jan Henning <jh+vsc-hg@buttercookie.de>
2024-01-06 19:02:11 +0100

    New development version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "de.buttercookie.units"
         minSdk = 4
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.1"
+        versionCode = 11
+        versionName = "1.2a1"
     }
 
     buildFeatures {
